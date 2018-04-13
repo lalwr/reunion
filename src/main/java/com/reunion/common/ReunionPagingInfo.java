801dfe977c641fc5a7d940d93187cf2dbf16f206
lalwr <lalwrj@gmail.com>
2018-04-13 17:57:14 +0900

    feat : lombok 설정 추가

--- src/main/java/com/reunion/common/ReunionPagingInfo.java
+++ src/main/java/com/reunion/common/ReunionPagingInfo.java
@@ -1,41 +1,14 @@
 package com.reunion.common;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ReunionPagingInfo {
 
     private int page = 1;
-    private int pageVal = 0;
     private int pageSize = 3;
     private int totCount;
 
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
-
-    public int getPageVal() {
-        return pageVal;
-    }
-
-    public void setPageVal(int pageVal) {
-        this.pageVal = pageVal;
-    }
-
-    public int getPageSize() {
-        return pageSize;
-    }
-
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
-    }
-
-    public int getTotCount() {
-        return totCount;
-    }
-
-    public void setTotCount(int totCount) {
-        this.totCount = totCount;
-    }
 }
