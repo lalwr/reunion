0b93bf08f714b963ca5b7236e8a350693a11ed15
Life4honor <choejy0329@gmail.com>
2018-04-25 00:00:16 +0900

    refactor: Change SignUpService -> ManagingService
    refactoring Service. SignUpService to ManagingService('profile update' feature dev is on going)

--- src/main/java/com/reunion/service/ManagingServiceImpl.java
+++ src/main/java/com/reunion/service/ManagingServiceImpl.java
@@ -18,13 +18,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.Locale;
 import java.util.UUID;
 
 @Service
-public class SignUpServiceImpl implements SignUpService{
+public class ManagingServiceImpl implements ManagingService {
     @Autowired
     MemberSchoolDao memberSchoolDao;
     @Autowired
@@ -42,8 +42,8 @@ public class SignUpServiceImpl implements SignUpService{
         member.setId(id);
         member.setPassword(password);
         member.setName(name);
-        member.setRegDate(new SimpleDateFormat("yyyy-MM-dd").format(date));
-        member.setEditDate(new SimpleDateFormat("yyyy-MM-dd").format(date));
+        member.setRegDate(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss",Locale.KOREA).format(date));
+        member.setEditDate(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA).format(date));
         int memberNo = memberDao.insertMember(member);
         int schoolNo = schoolDao.selectSchool(school).getNo();
         MemberSchool ms = new MemberSchool();
@@ -57,7 +57,7 @@ public class SignUpServiceImpl implements SignUpService{
     public void update(String memberId, String password,String school) {
         Member member = memberDao.selectMember(memberId);
         member.setPassword(ShaEncoding.cryptedPwd(password));
-        member.setEditDate(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+        member.setEditDate(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         int memberNo = memberDao.updateMember(member);
         int schoolNo = schoolDao.selectSchool(school).getNo();
         MemberSchool ms = memberSchoolDao.selectMemberSchool(member.getNo());
@@ -80,6 +80,85 @@ public class SignUpServiceImpl implements SignUpService{
     public ProfPicDTO getProfile(int memberNo){
         return profPicDao.profileDetail(memberNo);
     }
+
+    @Override
+    @Transactional
+    public void profileUpdate(String memberId, MultipartFile file){
+        Member member = memberDao.selectMember(memberId);
+        ProfPicDTO profile = profPicDao.profileDetail(member.getNo());
+
+        profPicDao.delete(member.getNo());
+
+        int year = LocalDate.now().getYear();
+        int month = LocalDate.now().getMonthValue();
+        int day = LocalDate.now().getDayOfMonth();
+//        Calendar cal = Calendar.getInstance();
+//        cal.get(Calendar.YEAR);
+//        cal.get(Calendar.MONTH) + 1; // 월은 0부터 시작
+//        cal.get(Calendar.DAY_OF_MONTH);
+
+
+        // 윈도우의 경우엔 디렉토리 구분자가 \
+        // unix계열은 디렉토리 구분자가 /
+        // File.separator 를 이용하여 디렉토리를 구분한다.
+        StringBuffer sb = new StringBuffer("/Users/life4honor/Downloads/profPic/");
+        sb.append(year);
+        sb.append("/");
+        sb.append(month);
+        sb.append("/");
+        sb.append(day);
+        sb.append("/");
+
+        String dir = sb.toString();
+
+        File fileObj = new File(dir);
+        if(!fileObj.exists()){ // 해당 디렉토리가 존재하지 않는다면
+            fileObj.mkdirs(); // 하위 폴더까지 생성한다.
+        }
+
+        UUID uuid = UUID.randomUUID();
+        String saveFileName = uuid.toString();  // 파일명
+        String saveFilePath = dir + saveFileName; // 디렉토리 + 파일명
+
+        //file.getBytes() // 망하자.
+        InputStream in = null;
+        FileOutputStream out = null;
+        try {
+            in = file.getInputStream();
+            out = new FileOutputStream(saveFilePath);
+            byte[] buffer = new byte[1024];
+            int readCount = 0;
+            // 만약 파일길이가 1026
+            // 1024
+            // 2
+            while((readCount = in.read(buffer)) != -1){ // -1(EoF)
+                out.write(buffer, 0, readCount);
+            }
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String originFileName = file.getOriginalFilename();
+        profile.setName(originFileName);
+        profile.setTempName(saveFileName);
+        profile.setFileSize(file.getSize());
+        profile.setPath(dir);
+        profile.setFormat(file.getContentType());
+        profile.setRegDate(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA).format(new Date()));
+        profPicDao.upload(profile);
+
+    }
     @Override
     @Transactional
     public void profile(String memberId, MultipartFile file){
@@ -88,10 +167,11 @@ public class SignUpServiceImpl implements SignUpService{
         // 하나의 폴더에 너무 많은 파일이 저장되면 관리하기 어렵다.
         // /tmp/helloboard 에 저장하도록 한다.
         // /tmp/helloboard/년/월/일/uuid
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH) + 1; // 월은 0부터 시작
-        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int year = LocalDate.now().getYear();
+        int month = LocalDate.now().getMonthValue();
+        int day = LocalDate.now().getDayOfMonth();
+
 
         // 윈도우의 경우엔 디렉토리 구분자가 \
         // unix계열은 디렉토리 구분자가 /
